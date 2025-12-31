5138377d8e05fb114992d08fc0cdf536901ba081
Ann Naser Nabil <ann.n.nabil@gmail.com>
2026-01-01 03:29:24 +0600

    added

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,4 @@
-export const PAGE_SIZE = 8;
+export const PAGE_SIZE = 30;
 
 export const LIGHT_MODE = "light",
 	DARK_MODE = "dark",
@@ -6,12 +6,13 @@ export const LIGHT_MODE = "light",
 export const DEFAULT_THEME = AUTO_MODE;
 
 // Banner height unit: vh
-export const BANNER_HEIGHT = 35;
-export const BANNER_HEIGHT_EXTEND = 30;
-export const BANNER_HEIGHT_HOME = BANNER_HEIGHT + BANNER_HEIGHT_EXTEND;
+// Banner height unit: vh
+export const BANNER_HEIGHT = 50;  // 35% of viewport height
+export const BANNER_HEIGHT_EXTEND = 20;  // Additional 30% for extended effect
+export const BANNER_HEIGHT_HOME = BANNER_HEIGHT + BANNER_HEIGHT_EXTEND;  // Total 65% for home
 
 // The height the main panel overlaps the banner, unit: rem
-export const MAIN_PANEL_OVERLAPS_BANNER_HEIGHT = 3.5;
+export const MAIN_PANEL_OVERLAPS_BANNER_HEIGHT = 1.5;
 
 // Page width: rem
-export const PAGE_WIDTH = 75;
+export const PAGE_WIDTH = 100;
